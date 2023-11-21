56ef97ea3c8128a772c71a55cd894d4b1c8c9177
Zenxly <113883335+Zenxly@users.noreply.github.com>
2023-11-21 14:59:56 +0800

    Add files via upload

---
+++ admin_page.php
@@ -0,0 +1,30 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+if(!isset($_SESSION['admin_name'])){
+   header('location:login_form.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Jadwal Kebersihan</title>
+  <!--  -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="stylesheet" href="/testimoni/preloader.css">
+  <!-- icon -->
+  <link rel="icon" type="image/x-icon" href="/testimoni/LOGO/PNG - LOGO PENGADILAN AGAMA 1.png">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://unpkg.com/feather-icons"></script>
+  <!-- body -->
+<body>  
+  <h1>admin page</h1>
+</body>
+</html>
